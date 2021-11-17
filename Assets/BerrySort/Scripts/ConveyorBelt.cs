2bc2b52df9a8b0c823ccce6e9d45598426626210
jogrobe <jonas.grobe@tu-dortmund.de>
2021-11-17 11:20:34 +0000

    Revert "Merge branch 'master' into 'develop'"
    This reverts merge request !3

--- Assets/BerrySort/Scripts/ConveyorBelt.cs
+++ Assets/BerrySort/Scripts/ConveyorBelt.cs
@@ -6,25 +6,10 @@ public class ConveyorBelt : MonoBehaviour
 
 
 {
+
     public GameObject belt;
     public Transform endpoint;
     public float speed;
-    public float animSpeed;
-    private Renderer beltRender;
-    private float offsetX = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        beltRender = GetComponent<Renderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        offsetX = (offsetX + Time.deltaTime * animSpeed) % 1;
-        beltRender.material.mainTextureOffset = new Vector2(offsetX, 0f);
-    } 
 
     void OnTriggerStay(Collider other)
     {
@@ -32,4 +17,15 @@ public class ConveyorBelt : MonoBehaviour
         other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
 
     }
+    /*     // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        } */
 }
