2c4c36a46feca8fbc9cc5ad7ec7a12f9c8aea8e7
jogrobe <jonas.grobe@tu-dortmund.de>
2022-02-07 16:35:31 +0100

    Fixed reset button and berry display

---
+++ Assets/BerryDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Berries;
+
+public class BerryDisplay : MonoBehaviour
+{
+    public GameObject image;
+    public Transform panel;
+    public List<GameObject> images = new List<GameObject>();
+    public int limit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.GetComponent<RawImage>().enabled = false;
+        images.Add(image);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addBerry(Berry berry, Color outlineColor) {
+        GameObject new_image = Instantiate(images.Last(), panel);
+        new_image.GetComponent<Outline>().effectColor = outlineColor;
+        foreach (GameObject go in images)
+        {
+            go.GetComponent<RectTransform>().localPosition += Vector3.right * 160;
+        }
+        Texture2D texture = new Texture2D(16, 16);
+        byte[] b64_bytes = System.Convert.FromBase64String(berry.image);
+        texture.LoadImage(b64_bytes);
+        new_image.GetComponent<RawImage>().enabled = true;
+        new_image.GetComponent<RawImage>().texture = texture;
+        images.Add(new_image);
+        if (images.Count > limit)
+        {
+            Destroy(images[0]);
+            images.RemoveAt(0);
+        }
+    }
+}
--- Assets/BerrySort/Scripts/GameManager.cs
+++ Assets/BerrySort/Scripts/GameManager.cs
@@ -8,25 +8,13 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.PauseGame();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-    }
-
-    public void ResumeGame()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
     }
 
     public void ReloadGame()
